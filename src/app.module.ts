9bdcc080f6c99406d5b0428a121c03f25fe58e57
cuter97 <aguscastets@gmail.com>
2023-07-30 17:43:44 -0300

    add auth module and user.entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,6 +17,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: process.env.STATE === 'dev' ? true : false
     }),
+
+    AuthModule,
   ],
   controllers: [],
   providers: [],
