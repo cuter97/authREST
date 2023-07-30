22e30820936a7c55d2a9b502bbfd7949544961cd
cuter97 <aguscastets@gmail.com>
2023-07-30 19:00:01 -0300

    create register user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const logger = new Logger('Bootstrap');
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT);
+  logger.log(`App running on PORT: ${process.env.PORT}`)
 }
 bootstrap();
