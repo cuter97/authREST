056f9719417ce6e4f04475ec1fd5b30b1e01f82b
cuter97 <aguscastets@gmail.com>
2023-08-10 17:39:15 -0300

    emit from server

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,12 +1,13 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
 import { Socket, Server } from 'socket.io';
+import { NewMessageDto } from './dto/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     @WebSocketServer() wss: Server;
-    
+
     constructor(
         private readonly messagesWsService: MessagesWsService
     ) { }
@@ -15,10 +16,32 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
         this.messagesWsService.registerClient(client);
         this.wss.emit('clients-updated', this.messagesWsService.getConnectdClients());
     }
-    
+
     handleDisconnect(client: Socket) {
         this.messagesWsService.removeClient(client.id);
         this.wss.emit('clients-updated', this.messagesWsService.getConnectdClients());
+    }
+
+    @SubscribeMessage('message-from-client')
+    onMessageClient(client: Socket, payload: NewMessageDto) {
+
+        //! emite unicamente al cliente.
+        // client.emit('message-from-server', {
+        //     fullName: 'Soy Yo xd',
+        //     message: payload.message || 'no-message'
+        // });
+
+        //! emitir a todos MENOS al cliente inicial
+        // client.broadcast.emit('message-from-server', {
+        //     fullName: 'Soy Yo xd',
+        //     message: payload.message || 'no-message'
+        // });
+
+        //! todos los clientes reciben la info
+        this.wss.emit('message-from-server', {
+            fullName: client.id,
+            message: payload.message || 'no-message'
+        });
     }
 
 }
