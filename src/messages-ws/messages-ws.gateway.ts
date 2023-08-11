2052904531f8c6302b23620d9058c98ace0d6eb7
cuter97 <aguscastets@gmail.com>
2023-08-11 18:12:38 -0300

    validate jwt handshake, connect socket with user

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,9 @@
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { JwtService } from '@nestjs/jwt';
 import { MessagesWsService } from './messages-ws.service';
 import { Socket, Server } from 'socket.io';
 import { NewMessageDto } from './dto/new-message.dto';
+import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -9,11 +11,23 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
     @WebSocketServer() wss: Server;
 
     constructor(
-        private readonly messagesWsService: MessagesWsService
+        private readonly messagesWsService: MessagesWsService,
+        private readonly jwtService: JwtService,
     ) { }
 
-    handleConnection(client: Socket) {
-        this.messagesWsService.registerClient(client);
+    async handleConnection(client: Socket) {
+        const token = client.handshake.headers.authentication as string;
+        let payload: JwtPayload;
+
+        try {
+            payload = this.jwtService.verify(token);
+            await this.messagesWsService.registerClient(client, payload.id);
+            
+        } catch (error) {
+            client.disconnect();
+            return;
+        }
+
         this.wss.emit('clients-updated', this.messagesWsService.getConnectdClients());
     }
 
@@ -39,7 +53,7 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
 
         //! todos los clientes reciben la info
         this.wss.emit('message-from-server', {
-            fullName: client.id,
+            fullName: this.messagesWsService.getUserFullName(client.id),
             message: payload.message || 'no-message'
         });
     }
--- src/messages-ws/messages-ws.module.ts
+++ src/messages-ws/messages-ws.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { MessagesWsService } from './messages-ws.service';
 import { MessagesWsGateway } from './messages-ws.gateway';
 
 @Module({
-  providers: [MessagesWsGateway, MessagesWsService]
+  providers: [MessagesWsGateway, MessagesWsService],
+  imports: [AuthModule]
 })
 export class MessagesWsModule {}
